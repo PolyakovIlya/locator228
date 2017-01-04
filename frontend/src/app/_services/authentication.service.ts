6910ef27175667adf7bdaf2408e25cf0715e1b87
ilya.polyakov <9213539@gmail.com>
2017-01-04 23:00:41 +0200

    Wrote auth service with login and logout methods.

--- frontend/src/app/_services/authentication.service.ts
+++ frontend/src/app/_services/authentication.service.ts
@@ -1,3 +1,39 @@
 /**
  * Created by ilyapolyakov on 1/4/17.
  */
+import {Injectable} from '@angular/core'
+import {Http, Headers, Response} from '@angular/http'
+import {Observable} from 'rxjs'
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class AuthenticationService {
+    public token: string;
+
+    constructor(private http: Http) {
+        var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        this.token = currentUser && currentUser.token;
+    }
+
+    login(username: string, password: string): Observable<boolean> {
+        return this.http.post('api/authentificate', JSON.stringify({username: username, password: password}))
+            .map((response: Response) => {
+                let token = response.json() && response.json().token;
+                if(token) {
+                    this.token = token;
+                    //if we get token from backend - store it to local
+                    localStorage.setItem('currentUser', JSON.stringify({username: username, token: token}));
+
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+    }
+
+    logout(): void {
+        this.token = null;
+        localStorage.removeItem('currentUser');
+    }
+
+}
